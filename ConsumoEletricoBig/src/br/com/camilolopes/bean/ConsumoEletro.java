6fd576fbf248ef9e1a1ae37519844eeeae1f6ad0
camilolopes <camilolopes@camilo>
2012-07-14 23:39:55 -0300

    consumo eletrico big TDD na prática  SRC

---
+++ ConsumoEletricoBig/src/br/com/camilolopes/bean/ConsumoEletro.java
@@ -0,0 +1,7 @@
+package br.com.camilolopes.bean;
+
+
+public class ConsumoEletro {
+		
+
+}
---
+++ ConsumoEletricoBig/src/br/com/camilolopes/teste/ConsumoEletricoTeste.java
@@ -0,0 +1,14 @@
+package br.com.camilolopes.teste;
+
+import static org.junit.Assert.*;
+
+import java.math.BigDecimal;
+
+import org.junit.Test;
+
+import br.com.camilolopes.bean.ConsumoEletro;
+
+public class ConsumoEletricoTeste {
+
+	
+}
